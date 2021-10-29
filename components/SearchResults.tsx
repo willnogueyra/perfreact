c127b2dddff861ebb74ee639229ce86061bfe4e5
William Nogueira <will.ribeiro@live.com>
2021-10-29 00:24:14 -0300

    virtualização

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,4 @@
-
+import { List, ListRowRenderer } from "react-virtualized"
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -13,15 +13,31 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({totalPrice, results, onAddToWishlist}: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style}) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem  
+          product={results[index]} 
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product => {
-        return (
-          <ProductItem key={product.id} product={product} onAddToWishlist={onAddToWishlist}/>
-        )
-      })}
+
+      <List
+        height={300} 
+        rowHeight={30} // tamanho da linha
+        width={900}
+        overscanRowCount={5} // quantidade de itens carregados para scroll
+        rowCount={results.length} // quantidade total de itens
+        rowRenderer={rowRenderer} // função 
+      />
+
+      
     </div>
   );
 }
